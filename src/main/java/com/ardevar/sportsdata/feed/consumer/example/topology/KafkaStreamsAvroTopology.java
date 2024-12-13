7b6bc29c593a4fb16ab97684bc0a2d1938de4d19
ardvarltd <167325892+ardvarltd@users.noreply.github.com>
2024-12-13 13:15:48 +0200

    Add new market/settlement fields

--- src/main/java/com/ardevar/sportsdata/feed/consumer/example/topology/KafkaStreamsAvroTopology.java
+++ src/main/java/com/ardevar/sportsdata/feed/consumer/example/topology/KafkaStreamsAvroTopology.java
@@ -26,46 +26,62 @@ import stream.avro.sport.translations.Value;
 @ConditionalOnProperty(value = "input.data.type", havingValue = "avro", matchIfMissing = false)
 public class KafkaStreamsAvroTopology {
 
+  private final SportsDataConfig sportsDataConfig;
+  private final StreamsBuilderFactoryBean streamsBuilderFactoryBean;
+  private final SpecificAvroSerde<SettlementSportsStreamMessage>
+      settlementSportsStreamMessageSpecificAvroSerde;
+  private final SpecificAvroSerde<EventSportsStreamMessage>
+      eventSportsStreamMessageSpecificAvroSerde;
+  private final SpecificAvroSerde<MarketSportsStreamMessage>
+      marketSportsStreamMessageSpecificAvroSerde;
+  private final SpecificAvroSerde<Value> enumerationAvroSerde;
 
-    private final SportsDataConfig sportsDataConfig;
-    private final StreamsBuilderFactoryBean streamsBuilderFactoryBean;
-    private final SpecificAvroSerde<SettlementSportsStreamMessage> settlementSportsStreamMessageSpecificAvroSerde;
-    private final SpecificAvroSerde<EventSportsStreamMessage> eventSportsStreamMessageSpecificAvroSerde;
-    private final SpecificAvroSerde<MarketSportsStreamMessage> marketSportsStreamMessageSpecificAvroSerde;
-    private final SpecificAvroSerde<Value> enumerationAvroSerde;
+  @PostConstruct
+  public void defineTopology() throws Exception {
 
+    StreamsBuilder builder = streamsBuilderFactoryBean.getObject();
+    assert builder != null;
 
-    @PostConstruct
-    public void defineTopology() throws Exception {
+    streamsBuilderFactoryBean.setStreamsUncaughtExceptionHandler(new UncaughtExceptionHandler());
 
-        StreamsBuilder builder = streamsBuilderFactoryBean.getObject();
-        assert builder != null;
+    builder.stream(
+            sportsDataConfig.getComputedEventTopic(),
+            Consumed.with(Serdes.String(), eventSportsStreamMessageSpecificAvroSerde))
+        .peek(
+            (key, value) ->
+                log.info(
+                    "Got Record From Event Stream with id {} - {}",
+                    key,
+                    value != null ? value.toString() : null));
 
-        streamsBuilderFactoryBean.setStreamsUncaughtExceptionHandler(new UncaughtExceptionHandler());
+    builder.stream(
+            sportsDataConfig.getComputedMarketTopic(),
+            Consumed.with(Serdes.String(), marketSportsStreamMessageSpecificAvroSerde))
+        .peek(
+            (key, value) ->
+                log.info(
+                    "infoGot Record From Market Stream with id {} - {}",
+                    key,
+                    value != null ? value.toString() : null));
 
-        builder.stream(sportsDataConfig.getComputedEventTopic(), Consumed.with(Serdes.String(), eventSportsStreamMessageSpecificAvroSerde))
+    builder.stream(
+            sportsDataConfig.getComputedSettlementTopic(),
+            Consumed.with(Serdes.String(), settlementSportsStreamMessageSpecificAvroSerde))
+        .peek(
+            (key, value) ->
+                log.info(
+                    "Got Record From Settlement Stream with id {} - {}",
+                    key,
+                    value != null ? value.toString() : null));
 
-                .peek((key, value) -> {
-                            log.info("Got Record From Event Stream with id {} - {}", key, value.toString());
-                        }
-                );
-
-        builder.stream(sportsDataConfig.getComputedMarketTopic(), Consumed.with(Serdes.String(), marketSportsStreamMessageSpecificAvroSerde))
-                .peek((key, value) -> {
-                    log.info("Got Record From Market Stream with id {} - {}", key, value.toString());
-                });
-
-        builder.stream(sportsDataConfig.getComputedSettlementTopic(), Consumed.with(Serdes.String(), settlementSportsStreamMessageSpecificAvroSerde))
-                .peek((key, value) -> {
-                    log.info("Got Record From Settlement Stream with id {} - {}", key, value.toString());
-                });
-
-        builder.stream(sportsDataConfig.getComputedEnumerationTopic(),
-                        Consumed.with(Serdes.String(), enumerationAvroSerde))
-                .peek((key, value) -> {
-                    log.info("Got Record From Enumerations Stream with id {} - {}", key,
-                            value.toString());
-                });
-    }
-
+    builder.stream(
+            sportsDataConfig.getComputedEnumerationTopic(),
+            Consumed.with(Serdes.String(), enumerationAvroSerde))
+        .peek(
+            (key, value) ->
+                log.info(
+                    "Got Record From Enumerations Stream with id {} - {}",
+                    key,
+                    value != null ? value.toString() : null));
+  }
 }
